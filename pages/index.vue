91b2c1633ca6ab874a8e7f418d4d002b5bf6a57d
David Asensio Cañas <david.asensio@icloud.com>
2020-10-13 19:58:17 +0200

    Configure SCSS preprocessor

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
+<style scoped lang="scss">
+.title {
+  font-weight: 400;
+  color: palevioletred; 
+
+  &__bold {
+    font-weight: 700;
+  }
+}
+</style>
+
 <template>
-  <h1>Hello world!</h1>
+  <h1 class="title">Hello <span class="title__bold">world!</span></h1>
 </template>
