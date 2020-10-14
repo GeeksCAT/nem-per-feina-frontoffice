e1a5084ab5b7332d805a2f0b9e4c5536ccd1effc
David Asensio Cañas <david.asensio@icloud.com>
2020-10-14 20:27:46 +0200

    Add simple Button Component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,21 @@
-<style scoped lang="scss">
-.title {
-  font-weight: 400;
-  color: palevioletred; 
-
-  &__bold {
-    font-weight: 700;
-  }
-}
-</style>
 
 <template>
-  <h1 class="title">Hello <span class="title__bold">world!</span></h1>
+  <Button
+    label="Click me!"
+    @on-click="onButtonClick"
+  />
 </template>
+
+
+<script>
+import { Button } from '../components/Button'
+
+export default {
+  components: { Button },
+  methods: {
+    onButtonClick() {
+      alert('Button have been clicked')
+    },
+  },
+}
+</script>
