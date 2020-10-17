507e74c2b14ab41f4676dc42be60b081043f8586
Marc CM <mcmontseny@gmail.com>
2020-10-17 13:40:23 +0200

    Fix lint errors

--- src/components/Header/Header.stories.js
+++ src/components/Header/Header.stories.js
@@ -1,13 +1,13 @@
-import { Header } from "."
+import { Header } from '.'
 
 export default {
   title: 'Components/Header',
   component: Header
-};
+}
 
 const Template = () => ({
   components: { Header },
   template: '<Header />'
-});
+})
 
 export const Default = Template.bind({})
