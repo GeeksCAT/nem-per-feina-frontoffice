6d3127d4ccee751f2458928437c82b4f2ff966eb
Marc CM <mcmontseny@gmail.com>
2020-10-19 22:58:28 +0200

    Add Icon

---
+++ src/components/Icon/Icon.stories.js
@@ -0,0 +1,19 @@
+import { Icon } from '.'
+
+export default {
+  title: 'Components/Icon',
+  component: Icon
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Icon },
+  template: '<Icon v-bind="$props" />'
+})
+
+export const Default = Template.bind({})
+Default.args = {
+  iconName : 'geolocation',
+  size: 24,
+  iconColor: 'black'
+}
