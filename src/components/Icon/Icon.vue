a85ba97d0ae7bfd4a747a5eb4587d6cc04b9cd25
Marc CM <mcmontseny@gmail.com>
2020-10-19 22:58:28 +0200

    Add Icon

---
+++ src/components/Icon/Icon.vue
@@ -0,0 +1,41 @@
+<template>
+  <svg
+    class="Icon"
+    :width="size"
+    :height="size"
+    viewBox="0 0 24 24"
+    :aria-labelledby="iconName"
+  >
+    <use 
+      :href="`#${iconName}`" 
+      :style="{ fill: iconColor }" 
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    iconName: {
+      type: String,
+      default: 'geolocation'
+    },
+    size: {
+      type: [Number, String],
+      default: 24
+    },
+    iconColor: {
+      type: String,
+      default: 'black'
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  .Icon {
+    display: block;
+    flex-shrink: 0;
+    transition: fill 200ms;
+  }
+</style>
