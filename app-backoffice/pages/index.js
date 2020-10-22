52188c809596d217d2852d963beed3f34073745f
Eudald Dachs Gonzalez <eudago@MacBook-Pro-de-Eudald.local>
2020-10-22 20:01:39 +0200

    init nextjs

---
+++ app-backoffice/pages/index.js
@@ -0,0 +1,28 @@
+import styled, { createGlobalStyle } from 'styled-components'
+import normalize from 'styled-normalize'
+
+const GlobalStyles = createGlobalStyle`
+  ${normalize}
+  * {
+    box-sizing: border-box;
+
+    &::before,
+    &::after {
+      box-sizing: border-box;
+    }
+  }
+`
+const Wrapper = styled.div`
+  background: red;
+`
+
+function HomePage () {
+  return (
+    <>
+      <GlobalStyles />
+      <Wrapper>Welcome to Next.js!</Wrapper>
+    </>
+  )
+}
+
+export default HomePage
