2921a986969b0bb2738779aa2a7d2a2a20c4df84
Marc Casamitjana Montseny <72517550+mcmontseny@users.noreply.github.com>
2020-10-22 19:02:18 +0200

    Add Link Component (#39)
    Add Link Component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,6 +6,10 @@
     label="Click me!"
     @on-click="onButtonClick"
   />
+  <Link
+    label="Software engineer"
+    destination="/test"
+  />
   <Icon
     iconName="geolocation"
     size="24"
@@ -17,10 +21,11 @@
 <script>
 import { Button } from '../components/Button'
 import { Header } from '../components/Header'
+import { Link } from '../components/Link'
 import { Icon } from '../components/Icon'
 
 export default {
-  components: { Button, Header, Icon },
+  components: { Button, Header, Link, Icon },
   methods: {
     onButtonClick () {
       alert('Button have been clicked')
