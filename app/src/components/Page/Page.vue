ffbb514584d098549bfa5ef6369b36d077fe9201
Eudald Dachs Gonzalez <eudago@MacBook-Pro-de-Eudald.local>
2020-10-24 20:06:15 +0200

    fix lint

---
+++ app/src/components/Page/Page.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Header/>
+    <slot></slot>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import { Header } from '../Header'
+import { Footer } from '../Footer'
+
+export default {
+  name: 'Page',
+  components: { Header, Footer }
+}
+</script>
