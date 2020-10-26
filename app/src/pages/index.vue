764a5e0e763da04b52dc958cdabb78e4dc27f9e5
David Asensio Cañas <david.asensio@icloud.com>
2020-10-26 20:15:11 +0100

    Add home page

--- app/src/pages/index.vue
+++ app/src/pages/index.vue
@@ -1,39 +1,38 @@
 
 <template>
-<div>
-  <Button
-    label="Click me!"
-    @on-click="onButtonClick"
+<main class="OffersGrid">
+  <JobOfferCard
+    v-for="n in 10"
+    :key="n"
+    title="LEARN LARAVEL WITH VUE2"
+    type="intershipt"
+    typeVariant="warning"
+    location="DHAKA, BANGLADESH"
+    description="Some additional info for this screen about validity of the ads, etc. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante."
+    image="https://picsum.photos/id/237/350/233"
+    imageAlt="tes"
   />
-  <Link
-    label="Software engineer"
-    destination="/test"
-  />
-  <Icon
-    iconName="geolocation"
-    size="24"
-    iconColor="black"
-  />
-</div>
+</main>
 </template>
 
 <script>
-import { Button } from '../components/Button'
-import { Icon } from '../components/Icon'
-import { Link } from '../components/Link'
+import { JobOfferCard } from '../components/JobOfferCard'
 
 export default {
-  components: { Button, Link, Icon },
-  methods: {
-    onButtonClick () {
-      alert('Button have been clickede2332')
-    }
-  }
+  components: { JobOfferCard }
 }
 </script>
 
-<style>
+<style lang="scss">
 * {
   margin: 0;
+}
+
+.OffersGrid {
+  display: grid;
+  grid-gap: 2em;
+  grid-template-columns: repeat(3, 1fr);
+  padding: 2em;
+  width: 100%;
 }
 </style>
